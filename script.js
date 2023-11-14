c22ba3b04def0a80c6498abee9014005c193a4ae
Johan Söderlund <johan.soderlund96@gmail.com>
2023-11-14 11:55:59 +0100

    cleaned up some css and added comments

---
+++ script.js
@@ -0,0 +1,34 @@
+// Generera ett slumpmässigt nummer mellan 0 och 100
+const randomNumber = Math.floor(Math.random() * 101);
+
+// Antal chanser spelaren har
+let chances = 10; 
+
+function checkGuess() {
+  const guess = parseInt(document.getElementById('guessInput').value);
+
+  if (guess === randomNumber) {
+    showMessage('Grattis! Du har gissat rätt nummer!');
+  } else if (guess < randomNumber && guess > 0) {
+    showMessage('Talet är för lågt. Försök igen!');
+  } else if (guess > randomNumber && guess < 100) {
+    showMessage('Talet är för högt. Försök igen!');
+  } else {
+    showMessage('Vänligen ange ett nummer mellan 0 och 100.');
+  }
+
+  chances--;
+
+  if (chances === 0) {
+    showMessage(`Tyvärr, dina gissningar är slut. Rätt nummer var ${randomNumber}.`);
+    disableInput();
+  }
+}
+
+function showMessage(message) {
+  document.getElementById('message').innerText = message;
+}
+
+function disableInput() {
+  document.getElementById('guessInput').disabled = true;
+}

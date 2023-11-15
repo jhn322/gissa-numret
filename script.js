42c4173d5114bf123b7e39f0e3fe95c1f319e629
Johan Söderlund <johan.soderlund96@gmail.com>
2023-11-15 03:25:20 +0100

    lagt till kommentarer

--- script.js
+++ script.js
@@ -9,11 +9,14 @@ function showMessage(messageFeedback) {
 }
 
 function checkGuess() {
+  // Funktion för att plocka gissningar från inputfältet
   const guess = Number(document.getElementById('guessInput').value);
+  // Länkar till lista i HTML där gissningarna visas
   const guessList = (document.getElementById('guessList'));
+  // Skapar ett nytt listelement för gissningarna
   const showGuess = document.createElement('li'); 
 
-
+  // If-sats som meddelar användaren vid rätt & ogiltig samt om för låg eller hög inmatning
   if (guess === randomNumber) {
     showMessage('Grattis! Du gissade rätt nummer!');
   } else if (guess < 0 || guess > 100 || isNaN(guess)) {
@@ -24,16 +27,21 @@ function checkGuess() {
     showMessage('Talet är för högt. Gissa lägre!');
   }
 
+  // Visar användarens gissning i listan under knappen
   showGuess.textContent =`${guess}`;
   guessList.appendChild(showGuess);
 
+  // Minskar antalet gissningar med 1
   count--;
 
+  // Om antalet gissningar uppnåt utan rätt svar anges detta
   if (count === 0 && guess !== randomNumber) {
     showMessage(`Tyvärr, du har använt upp alla dina gissningar. Rätt nummer var ${randomNumber}.`);
+    // Avaktiverar inputfältet
     disableInput();
   }
   
+  // Avslutar spelet
     function disableInput() {
     document.getElementById('guessInput').disabled = true; // Avaktiverar gissningsinputfältet
   }

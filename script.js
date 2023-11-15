c219e0e8396323fefd3a8c3e50f47962ac760e8b
Johan Söderlund <johan.soderlund96@gmail.com>
2023-11-15 02:43:35 +0100

    angett variblar för räkning av matte, hämtar från Id samt skapar lista vid inputoch gjort en if-sats som skriver ut meddelande vid rätt/fel/ogiltig gissning

--- script.js
+++ script.js
@@ -24,37 +24,3 @@ function checkGuess() {
     showMessage('Talet är för högt. Gissa lägre!');
   }
 
-
-
-
-
-
-  // if (guess === randomNumber) {
-  //   showMessage('Grattis! Du har gissat rätt nummer!'); // Meddelande om rätt gissning
-  // } else if (guess < 0 || guess > 100 || isNaN(guess)) {
-  //   showMessage('Vänligen ange ett nummer mellan 0 och 100.'); // Meddelande om ogiltig gissning
-  // } else {
-  //   showMessage(guess < randomNumber ? 'Talet är för lågt. Försök igen!' : 'Talet är för högt. Försök igen!'); // Meddelande om gissningen är för låg eller för hög
-  // }
-
-  listItem.textContent = `Gissning: ${guess}`; // Visar användarens gissning i listan
-  guessList.appendChild(listItem);
-
-  count--; // Minskar antalet tillgängliga gissningar
-
-  if (count === 0) {
-    showMessage(`Tyvärr, du har använt alla dina chanser. Rätt nummer var ${randomNumber}.`); // Meddelande om slutet av gissningarna
-    disableInput(); // Avaktiverar gissningsinputfältet
-  }
-}
-
-function showMessage(messageFeedback) {
-  document.getElementById('message').innerText = messageFeedback; // Visar meddelandet för användaren
-}
-
-function disableInput() {
-  document.getElementById('guessInput').disabled = true; // Avaktiverar gissningsinputfältet
-}
-
-
-// listItem = showGuess
